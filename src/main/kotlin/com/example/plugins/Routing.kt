5a2bc40d3b025c50ac0913b11235eb9939ad0daa
Jimmy McBride <mcbride967@gmail.com>
2021-12-15 14:31:09 -0500

    added our images' folder as a static directory so client can view them

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -4,11 +4,16 @@ import com.example.routes.getAllHeroes
 import com.example.routes.root
 import io.ktor.routing.*
 import io.ktor.application.*
+import io.ktor.http.content.*
 
 fun Application.configureRouting() {
 
     routing {
         root()
         getAllHeroes()
+
+        static("/images") {
+            resources("images")
+        }
     }
 }
