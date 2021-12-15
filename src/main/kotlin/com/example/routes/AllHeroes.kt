5f380c49b07f51cf3bb408f216e29e3a9256f093
Jimmy McBride <mcbride967@gmail.com>
2021-12-15 14:29:24 -0500

    injected hero repository and called getAllHeroes from our route

--- src/main/kotlin/com/example/routes/AllHeroes.kt
+++ src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -1,19 +1,27 @@
 package com.example.routes
 
 import com.example.models.ApiResponse
+import com.example.repository.HeroRepository
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import org.koin.ktor.ext.inject
 import java.lang.IllegalArgumentException
 
 fun Route.getAllHeroes() {
+    val heroRepository: HeroRepository by inject()
+
     get("/boruto/heroes") {
         try {
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
             require(page in 1..5)
+
+            val apiResponse = heroRepository.getAllHeroes(page)
+
             call.respond(
-                message = ApiResponse(success = true, message = "Page number: $page")
+                message = apiResponse,
+                status = HttpStatusCode.OK
             )
         } catch (e: NumberFormatException) {
             call.respond(
