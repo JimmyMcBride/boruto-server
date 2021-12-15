d789410c7d0b8003d018972610308c23fbb8398e
Jimmy McBride <mcbride967@gmail.com>
2021-12-15 12:04:20 -0500

    created error handling responses for page query

---
+++ src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -0,0 +1,33 @@
+package com.example.routes
+
+import com.example.models.ApiResponse
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import java.lang.IllegalArgumentException
+
+fun Route.getAllHeroes() {
+    get("/boruto/heroes") {
+        try {
+            val page = call.request.queryParameters["page"]?.toInt() ?: 1
+            require(page in 1..5)
+            call.respond(
+                message = ApiResponse(success = true, message = "Page number: $page")
+            )
+        } catch (e: NumberFormatException) {
+            call.respond(
+                message = ApiResponse(success = false, message = "Only numbers allowed."),
+                status = HttpStatusCode.BadRequest
+            )
+        } catch (e: IllegalArgumentException) {
+            call.respond(
+                message = ApiResponse(
+                    success = false,
+                    message = "Page number not in range of possible pages. Try 1 through 5."
+                ),
+                status = HttpStatusCode.BadRequest
+            )
+        }
+    }
+}
